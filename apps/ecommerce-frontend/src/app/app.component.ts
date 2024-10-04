50c9530f3efcfc3b59980355b324263c3de26991
TuanThanh0611 <nguyenhatuanthanh1901@gmail.com>
2024-10-04 11:28:12 +0700

    Generate NX + Angular

---
+++ apps/ecommerce-frontend/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import {Component, inject, OnInit} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {FaConfig, FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {fontAwesomeIcons} from "./share/font-awesome-icons";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {FooterComponent} from "./layout/footer/footer.component";
+
+@Component({
+  standalone: true,
+  imports: [RouterModule, FaIconComponent, NavbarComponent, FooterComponent],
+  selector: 'ecommerce-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig=inject(FaConfig);
+  ngOnInit() {
+    this.initFontAwesome();
+  }
+  private initFontAwesome(){
+    this.faConfig.defaultPrefix='far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons)
+  }
+}
